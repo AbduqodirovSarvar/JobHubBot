b27c3c5d8d8665ead61038d1a7de8f54b18d8d61
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2023-12-16 18:24:04 +0500

    add

--- Program.cs
+++ Program.cs
@@ -2,65 +2,90 @@ using JobHubBot.Controllers;
 using JobHubBot.Db.DbContexts;
 using JobHubBot.Models.Telegram;
 using JobHubBot.Services.Configurations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using System;
 
-var applicationBuilder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-// Services are added to the container here.
-applicationBuilder.Services.AddControllers();
-applicationBuilder.Services.Services(applicationBuilder.Configuration);
-applicationBuilder.Services.AddEndpointsApiExplorer();
-applicationBuilder.Services.AddSwaggerGen();
+// Configure services
+builder.Services.AddControllers();
+builder.Services.Services(builder.Configuration); // Assuming there's a method named "Services" in your extension method
 
-var application = applicationBuilder.Build();
-
-application.UseCors(options =>
+// Swagger configuration
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
 {
-    options.AllowAnyOrigin()
-        .AllowAnyMethod()
-        .AllowAnyHeader();
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "JobHubBot API", Version = "v1" });
 });
 
+// builder.Host.CreateDefaultBuilder(args)
+//     .ConfigureWebHostDefaults(webBuilder =>
+//     {
+//         webBuilder.UseStartup<Startup>();
+//         webBuilder.UseUrls("http://0.0.0.0:80", "https://0.0.0.0:443");
+//     });
 
-application.UseRequestLocalization(application.Services.GetService<IOptions<RequestLocalizationOptions>>().Value);
+var app = builder.Build();
 
-// HTTP request pipeline configuration.
-application.UseSwagger();
-application.UseSwaggerUI();
-
-application.UseRouting();
-
-application.UseEndpoints(endpoints =>
+// Enable CORS
+app.UseCors(options =>
 {
+    options.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+});
+
+app.UseRequestLocalization(app.Services.GetService<IOptions<RequestLocalizationOptions>>().Value);
+
+// Configure Swagger
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "JobHubBot API v1");
+    c.RoutePrefix = "swagger";
+});
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    // Get controller and action names
     var controllerName = typeof(BotController).Name.Replace("Controller", "", StringComparison.Ordinal);
     var actionName = typeof(BotController).GetMethods()[0].Name;
 
-    string? pattern = applicationBuilder.Configuration.GetSection(BotConfiguration.RouteSection).Value;
+    // Get route pattern from configuration
+    string? pattern = builder.Configuration.GetSection(BotConfiguration.RouteSection).Value;
 
+    // Map custom route
     endpoints.MapControllerRoute(
-            name: "jobohub",
-            pattern: pattern ?? "/api/bot",
-            defaults: new { controller = controllerName, action = actionName });
+        name: "jobohub",
+        pattern: pattern ?? "/api/bot",
+        defaults: new { controller = controllerName, action = actionName });
 
     endpoints.MapControllers();
 });
 
 try
 {
-    using var scope = application.Services.CreateScope();
+    // Apply migrations on startup
+    using var scope = app.Services.CreateScope();
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<AppDbContext>();
     context.Database.Migrate();
 }
 catch (Exception ex)
 {
-    Console.WriteLine($"Error adding migration: {ex.Message}");
+    Console.WriteLine($"Error applying migrations: {ex.Message}");
 }
 
+app.UseHttpsRedirection();
 
-application.UseHttpsRedirection();
+app.MapControllers();
 
-application.MapControllers();
-
-application.Run();
+app.Run();
